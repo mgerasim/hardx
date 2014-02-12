472b52173b5fe2ba2b3cc230fc1d5c8bef48e3d0
Mikhail Gerasimov <mgerasim@inbox.ru>
2014-02-13 10:54:37 +1100

    fix

--- deploy.bat
+++ deploy.bat
@@ -1,11 +1,6 @@
 git add . && git commit -m fix 
 git push origin master
 
-ConsoleWrite("Hello World!" & @CRLF)
-
-@Echo on
-Color 02
-Echo 'Text inside console'
 
 
 X:\HardX\src\HardX.git\git push origin master
