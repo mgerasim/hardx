d6e64cbcbfa9326506ee36787761037f2908c5ae
Mikhail Gerasimov <mgerasim@inbox.ru>
2014-03-20 11:41:13 +1100

    rooms

---
+++ HardX/HardX/Controllers/RoomController.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using HardX.Models;
+
+namespace HardX.Controllers
+{
+    public class RoomController : Controller
+    {
+        //
+        // GET: /Room/
+
+        public ActionResult Index()
+        {
+            Room model = new Room();
+            List<Room> theListModel = new List<Room>();
+            theListModel = (List<Room>)model.GetAll();
+
+            return View(theListModel);
+        }
+
+        //
+        // GET: /Room/Details/5
+
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        //
+        // GET: /Room/Create
+
+        public ActionResult Create()
+        {
+            RoomNew model = new RoomNew();
+            return View(model);
+        } 
+
+        //
+        // POST: /Room/Create
+
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                Room model = new Room();
+                model.Name = collection["Name"];
+                model.Save(model);
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+        
+        //
+        // GET: /Room/Edit/5
+ 
+        public ActionResult Edit(int id)
+        {
+            Room model = new Room();
+            model = model.GetById(id);
+            return View(model);
+        }
+
+        //
+        // POST: /Room/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                Room model = new Room();
+                model = model.GetById(id);
+                model.Name = collection["Name"];
+                model.Update(model);
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //
+        // GET: /Room/Delete/5
+ 
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        //
+        // POST: /Room/Delete/5
+
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+ 
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ HardX/HardX/Models/Room.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using HardX.Core;
+using System.ComponentModel.DataAnnotations;
+using HardX.Factories;
+
+namespace HardX.Models
+{
+    public class Room : Entity<Room>
+    {       
+        public Int32 ID {get; set;}
+        [Display(Name = "Полное имя")]
+        [Required(ErrorMessage = "* Укажите значение для полного имени")]
+        [StringLength(50, MinimumLength = 5, ErrorMessage = "* длина полного имени должна быть не менее чем 5 и не более чем 50 символов")]
+        [Editable(true)]
+        public virtual string Name { get; set; }
+
+        [Display(Name = "Здание")]
+        [Required(ErrorMessage = "* Укажите здание")]
+        public House House { get; set; }
+                                
+        public Room()
+        {
+            RoomFactory theFactory = new RoomFactory();
+            _repository = theFactory.createRepository();
+            if (_repository == null)
+                throw new NotImplementedException();
+        }       
+    }
+
+    public class RoomNew : Room
+    {
+        [Display(Name = "Полное имя")]
+        [Required(ErrorMessage = "* Укажите значение для полного имени")]
+        [StringLength(50, MinimumLength = 5, ErrorMessage = "* длина полного имени должна быть не менее чем 5 и не более чем 50 символов")]        
+        [Editable(true)]
+        public override string Name { get; set; }        
+    }
+}
