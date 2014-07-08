dcac5179080097237dfba30d4d807e900a6723e3
Mikhail Gerasimov <mgerasim@inbox.ru>
2014-07-08 17:16:14 +1100

    S0030-delete

---
+++ HardX/HardX/Controllers/DeviceController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using HardX.Models;
+
+namespace HardX.Controllers
+{
+    public class DeviceController : Controller
+    {
+        //
+        // GET: /Device/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        
+        //
+        // GET: /Device/Delete/5
+
+        public ActionResult Delete(int id)
+        {
+            Device item = new Device();
+            item = item.GetById(id);
+            item.Delete(item);
+            return View();
+        }
+    }
+}
